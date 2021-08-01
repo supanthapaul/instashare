7c69d20fa0b99294dc4011bde00334b9110ff446
Supantha Paul <supapaul56@gmail.com>
2021-08-01 11:59:48 +0530

    Initial Commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  Widget buildAuthScreen() {
+    return Text("Authenticated");
+  }
+
+  Widget buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.teal, Colors.purple]),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "InstaShare",
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 90,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => print("Hello world"),
+              child: Container(
+                width: 260,
+                height: 60,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage('assets/images/google_signin_button.png'),
+                  fit: BoxFit.cover,
+                )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
